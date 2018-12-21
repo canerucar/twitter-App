7288b34d3c796b74b69f8ed3f4436ee650d7104c
Caner Uçar <canerucar0@gmail.com>
2018-12-21 21:36:49 +0300

    pod dosyaları eklendi. LaunchScreen effectli yapıldı. RevealingSplashView Kullanıldı

--- twitter-App/twitter-App/ViewController.swift
+++ twitter-App/twitter-App/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import RevealingSplashView
+import LBTAComponents
 
 class ViewController: UIViewController {
 
+    //Initialize a revealing Splash with with the iconImage, the initial size and the background color
+    let revealingSplashView = RevealingSplashView(iconImage: UIImage(named: "twitteriCON")!, iconInitialSize: CGSize(width: 150 , height: 150), backgroundColor: UIColor(red:0.11, green:0.56, blue:0.95, alpha:1.0))
+
+    //Adds the revealing splash view as a sub view
+
+    
+    //Starts animation
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setupViews()
     }
 
+    
+    func setupViews() {
+        revealingSplashView.animationType = SplashAnimationType.squeezeAndZoomOut
+        revealingSplashView.startAnimation()
+            
+        
+        view.addSubview(revealingSplashView)
+    }
 
 }
 
