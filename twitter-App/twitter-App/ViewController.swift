74268d8449b82197cbef0322a98ad4740a20f117
Caner Uçar <canerucar0@gmail.com>
2018-12-22 18:32:09 +0300

    Ana Sayfa Auto Layout yapıldı. Navigation Bar eklendin. Hesap Oluştur sayfası eklendi.

--- twitter-App/twitter-App/ViewController.swift
+++ twitter-App/twitter-App/ViewController.swift
@@ -10,31 +10,52 @@ import UIKit
 import RevealingSplashView
 import LBTAComponents
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate {
 
     //Initialize a revealing Splash with with the iconImage, the initial size and the background color
     let revealingSplashView = RevealingSplashView(iconImage: UIImage(named: "twitteriCON")!, iconInitialSize: CGSize(width: 150 , height: 150), backgroundColor: UIColor(red:0.11, green:0.56, blue:0.95, alpha:1.0))
 
-    //Adds the revealing splash view as a sub view
-
-    
-    //Starts animation
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    
         
         setupViews()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        navigationController?.setNavigationBarHidden(false, animated: false)
+    }
     
     func setupViews() {
         revealingSplashView.animationType = SplashAnimationType.squeezeAndZoomOut
+        
+        //Diğer Efektler
+        //revealingSplashView.animationType = .woobleAndZoomOut
+        //revealingSplashView.animationType = .popAndZoomOut
+        //revealingSplashView.animationType = .rotateOut
+        //revealingSplashView.animationType = .swingAndZoomOut
+        //revealingSplashView.animationType = .twitter
+        
+         //Starts animation
         revealingSplashView.startAnimation()
             
         
         view.addSubview(revealingSplashView)
     }
-
+    @IBAction func baslaClicked(_ sender: Any) {
+        
+        //performSegue(withIdentifier: "baslaStoryboard", sender: nil)
+        
+    }
+    
 }
 
---
+++ twitter-App/twitter-App/hesapOlusturViewController.swift
@@ -0,0 +1,43 @@
+//
+//  hesapOlusturViewController.swift
+//  twitter-App
+//
+//  Created by Caner Uçar on 22.12.2018.
+//  Copyright © 2018 Caner Uçar. All rights reserved.
+//
+
+import UIKit
+
+class hesapOlusturViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+       
+        
+    }
+    
+   
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: false)
+        
+       
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+}
